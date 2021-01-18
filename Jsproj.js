b9254b932f2758508dabda82f9d48a8663c25672
Swarup Mohapatra <1930159@kiit.ac.in>
2021-01-19 01:55:48 +0530

    2nd commit

--- Jsproj.js
+++ Jsproj.js
@@ -1,1 +1,7 @@
 console.log("oniiii san tasukete kudasai");
+let x1="weebo";
+let x2="gamer"
+let x=`i am ${x1} ${x2} and I wanna die `;
+let y="i am "+x1+ " " +x2+" and I wanna die";
+console.log(x);
+console.log(y);
